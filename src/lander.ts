5900e4b4ec4a257526e8af39643ea701a7c6a4ef
Clemens Prerovsky <c.prerovsky@gentics.com>
2016-12-27 21:18:25 +0100

    implemented rotation

--- src/lander.ts
+++ src/lander.ts
@@ -2,10 +2,14 @@ const GRAVITY = -0.02;
 
 export class Vector {
     constructor(public x: number = 0, public y: number = 0) { }
-    rotate(angle) {
+    rotate(pivot: Vector, angle: number) {
+        // http://stackoverflow.com/questions/2259476/rotating-a-point-about-another-point-2d
+        // answer by six face
+        let sinA = Math.sin(angle);
+        let cosA = Math.cos(angle);
         return new Vector(
-            this.y * Math.sin(angle) + this.x * Math.cos(angle),
-            this.y * Math.cos(angle) - this.x * Math.sin(angle)
+            cosA * (this.x - pivot.x) - sinA * (this.y - pivot.y) + pivot.x,
+            sinA * (this.x - pivot.x) + cosA * (this.y - pivot.y) + pivot.y
         );
     }
 }
@@ -14,7 +18,7 @@ export default class Lander {
     width = 12
     height = 16
     thrust = 0.1
-    position = new Vector(50, 50);
+    position = new Vector(250, 250);
     angle = 0
     velocity = new Vector();
     engine: "off" | "half" | "full"
@@ -42,59 +46,61 @@ export default class Lander {
         this.velocity.y -= GRAVITY;
     }
 
-    protected getPoint(name: "bl" | "ul" | "ur" | "br"): Vector {
+    protected getPoint(name: "bl" | "tl" | "tr" | "br"): Vector {
         switch (name) {
             case "bl":
                 return new Vector(
                     this.position.x + this.width / -2,
                     this.position.y + this.height / 2)
-                    .rotate(this.angle);
+                    .rotate(this.position, this.angle);
             case "br":
                 return new Vector(
                     this.position.x + this.width / 2,
                     this.position.y + this.height / 2)
-                    .rotate(this.angle);
-            case "ul":
+                    .rotate(this.position, this.angle);
+            case "tl":
                 return new Vector(
                     this.position.x + this.width / -2.4,
                     this.position.y + this.height / -2)
-                    .rotate(this.angle);
-            case "ur":
+                    .rotate(this.position, this.angle);
+            case "tr":
                 return new Vector(
                     this.position.x + this.width / 2.4,
                     this.position.y + this.height / -2)
-                    .rotate(this.angle);
+                    .rotate(this.position, this.angle);
         }
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
         ctx.save();
         ctx.beginPath();
-        ctx.translate(this.position.x, this.position.y);
-        ctx.rotate(this.angle);
 
-        ctx.moveTo(this.width / -2, this.height / 2);
-        ctx.lineTo(this.width / -2.4, this.height / -2);
-        ctx.lineTo(this.width / 2.4, this.height / -2);
-        ctx.lineTo(this.width / 2, this.height / 2);
-        ctx.fillStyle = "grey";
-        ctx.fill();
+        let bl = this.getPoint("bl");
+        let tl = this.getPoint("tl");
+        let tr = this.getPoint("tr");
+        let br = this.getPoint("br");
+
+        ctx.moveTo(bl.x, bl.y);
+        ctx.lineTo(tl.x, tl.y);
+        ctx.lineTo(tr.x, tr.y);
+        ctx.lineTo(br.x, br.y);
+        ctx.lineTo(bl.x, bl.y);
+        ctx.strokeStyle = "grey";
+        ctx.stroke();
 
         // ctx.rect(this.width * -0.5, this.height * -0.5, this.width, this.height);
         ctx.closePath();
 
         // Draw the flame if engine is on
-        if (this.engine !== "off") {
-            ctx.beginPath();
-            ctx.moveTo(this.width * -0.5, this.height * 0.5);
-            ctx.lineTo(this.width * 0.5, this.height * 0.5);
-            ctx.lineTo(0, this.height * 0.5 + Math.random() * 10);
-            ctx.lineTo(this.width * -0.5, this.height * 0.5);
-            ctx.closePath();
-            ctx.fillStyle = "orange";
-            ctx.fill();
-        }
-        ctx.restore();
-
+        // if (this.engine !== "off") {
+        //     ctx.beginPath();
+        //     ctx.moveTo(this.width * -0.5, this.height * 0.5);
+        //     ctx.lineTo(this.width * 0.5, this.height * 0.5);
+        //     ctx.lineTo(0, this.height * 0.5 + Math.random() * 10);
+        //     ctx.lineTo(this.width * -0.5, this.height * 0.5);
+        //     ctx.closePath();
+        //     ctx.fillStyle = "orange";
+        //     ctx.fill();
+        // }
     }
 }
