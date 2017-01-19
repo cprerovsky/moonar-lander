ef34174993e38c6cea1a3ad971c8af0541c4c093
Clemens Prerovsky <c.prerovsky@gentics.com>
2017-01-19 17:01:49 +0100

    changed demo client impl

--- frontend/static/client.js
+++ frontend/static/client.js
@@ -1,99 +1,104 @@
-(function () {
-    "use strict";
-    var isFirst = false;
-    function Client(name) {
-        var name = name;
-        var token = '';
-        var ws = new WebSocket("ws://localhost:4711");
-        var first = isFirst;
-        isFirst = false;
-        ws.onerror = function (e) {
-            console.error(this.name, msg.data);
-        };
-        ws.onmessage = function (msg) {
-            let data = JSON.parse(msg.data);
-            console.log(name, msg.data);
-            if (data.token) {
-                token = data.token;
-            } else if (data.game === "start") {
-                console.log(name, msg.data);
-                if (!first) {
-                    ws.send(JSON.stringify({
-                        token: token, commands: [
-                            { engine: "full", rotation: "cw" },
-                            { rotation: "ccw", tick: 20 + rndoff() },
-                            { rotation: "off", tick: 25 + rndoff() },
-                            { engine: "off", tick: 150 + rndoff() },
-                            { engine: "full", tick: 180 + rndoff() },
-                            { engine: "off", rotation: "ccw", tick: 250 + rndoff() },
-                            { rotation: "off", tick: 260 + rndoff() },
-                            { engine: "full", tick: 350 + rndoff() },
-                            { engine: "off", tick: 500 + rndoff() },
-                            { engine: "full", tick: 700 + rndoff() },
-                            { engine: "off", tick: 1000 + rndoff() },
-                        ]
-                    }));
-                }
-            }
-        };
-        this.register = function () {
+"use strict";
+function Client(name, keyboard) {
+    var name = name;
+    var token = '';
+    var ws = new WebSocket("ws://localhost:4711");
+    var $out = document.getElementById('out');
+    $out.innerText += "new client: " + name + "\n";
+    console.log(name);
+    ws.onerror = function (e) {
+        console.error(this.name, msg.data);
+    };
+    ws.onmessage = function (msg) {
+        let data = JSON.parse(msg.data);
+        console.log(name, msg.data);
+        if (data.token) {
+            token = data.token;
+        } else if (data.game === "start" && !keyboard) {
+            ws.send(JSON.stringify({
+                token: token, commands: [
+                    { engine: "full", rotation: "cw" },
+                    { rotation: "ccw", tick: 20 + rndoff() },
+                    { rotation: "off", tick: 25 + rndoff() },
+                    { engine: "off", tick: 150 + rndoff() },
+                    { engine: "full", tick: 180 + rndoff() },
+                    { engine: "off", rotation: "ccw", tick: 250 + rndoff() },
+                    { rotation: "off", tick: 260 + rndoff() },
+                    { engine: "full", tick: 350 + rndoff() },
+                    { engine: "off", tick: 500 + rndoff() },
+                    { engine: "full", tick: 700 + rndoff() },
+                    { engine: "off", tick: 1000 + rndoff() },
+                ]
+            }));
+        }
+    };
+    ws.onopen = function () {
+        setTimeout(() => {
+            // omg this is ugly. timeout is needed to allow to server to send us a token after connecting
             ws.send(JSON.stringify({ token: token, name: name }));
-        }
-        function rndoff() { return Math.floor(Math.random() * 20) - 10 };
-        if (first) {
-            initKeyboardControls((cmd) => {
-                ws.send(JSON.stringify({
-                    token: token, commands: [cmd]
-                }));
-            });
-        }
+        }, 200);
     }
-
-    var clients = [
-        new Client("Katharina"),
-        new Client("Michael"),
-        new Client("Norbert"),
-        // new Client("Florian"),
-        // new Client("Clemens"),
-        // new Client("Klaus")
-    ];
-
-    setTimeout(() => {
-        clients.map((client) => client.register());
-    }, 1000);
-
-    function initKeyboardControls(cb) {
-        document.addEventListener('keyup', (e) => { cb(keyup(e)) });
-        document.addEventListener('keydown', (e) => { cb(keydown(e)) })
+    function rndoff() { return Math.floor(Math.random() * 20) - 10 };
+    if (keyboard) {
+        initKeyboardControls((cmd) => {
+            ws.send(JSON.stringify({
+                token: token, commands: [cmd]
+            }));
+        });
     }
+}
 
-    function keyup(event) {
-        switch (event.keyCode) {
-            case 37:
-            case 39:
-                // Right Arrow key
-                return { rotation: "off" };
-            case 38:
-            case 40:
-                // Up Arrow key
-                return { engine: "off" };
+var clientNames = ["Bernhard", "Clemens", "David", "Florian", "Joan", "Jan", "Johannes", "Leon", "Lisa", "Michael",
+    "Norbert", "Patrick", "Philipp", "Stefan", "Simon", "Sebastian", "Tawan", "Thomas"].sort(() => {
+        if (Math.random() > 0.5) {
+            return 1;
+        } else {
+            return -1
         }
-    }
+    });
 
-    function keydown(event) {
-        switch (event.keyCode) {
-            case 37:
-                // Left Arrow key
-                return { rotation: "ccw" };
-            case 39:
-                // Right Arrow key
-                return { rotation: "cw" };
-            case 38:
-                // Up Arrow key
-                return { engine: "full" };
-            case 40:
-                // Down
-                return { engine: "half" };
-        }
+function addClient() {
+    new Client(clientNames.pop());
+}
+
+function addKeyboardClient() {
+    new Client("KeyboardClient", true);
+}
+
+document.getElementById('addclient').addEventListener('click', addClient);
+document.getElementById('addkeyboardclient').addEventListener('click', addKeyboardClient);
+
+function initKeyboardControls(cb) {
+    document.addEventListener('keyup', (e) => { cb(keyup(e)) });
+    document.addEventListener('keydown', (e) => { cb(keydown(e)) })
+}
+
+function keyup(event) {
+    switch (event.keyCode) {
+        case 37:
+        case 39:
+            // Right Arrow key
+            return { rotation: "off" };
+        case 38:
+        case 40:
+            // Up Arrow key
+            return { engine: "off" };
     }
-})();
+}
+
+function keydown(event) {
+    switch (event.keyCode) {
+        case 37:
+            // Left Arrow key
+            return { rotation: "ccw" };
+        case 39:
+            // Right Arrow key
+            return { rotation: "cw" };
+        case 38:
+            // Up Arrow key
+            return { engine: "full" };
+        case 40:
+            // Down
+            return { engine: "half" };
+    }
+}
